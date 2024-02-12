7d60087fb5871dc3057b4d994d03992d12f25cb9
Bhavya Dhiman <bhavyadhiman7@gmail.com>
2024-02-13 05:00:56 +0530

    feat(string-calculator): handling of invalid/unsupported delimiters work in progress

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 class StringCalculator {
     constructor(string = '') {
-        this.delimeters = [',', '+', '*'];
+        this.delimeters = [','];
         this.dimensionalStringSplit = this.validateString(string);
         this.string = string || '';
     }
@@ -24,13 +24,66 @@ class StringCalculator {
         }
     }
 
+    validateAndReplaceDelimeterFromTheString(string = '') {
+        let stringWithoutAdditionalDelimeters = string;
+        if (string.startsWith('//')) {
+            this.delimeters = [];
+            const delimeterItemsString = string.substring(0, string.indexOf('\n'));
+            if (!delimeterItemsString.endsWith(']') || delimeterItemsString.startsWith('[')) {
+                throw new Error('Delimeter string should start and end with square brackets');
+            }
+            stringWithoutAdditionalDelimeters = string.substring(string.indexOf('\n') + 1);
+            const delimeterArray = delimeterItemsString.substring(2).split(']['); // ][ will be removed automatically
+            delimeterArray.forEach((delimeter, index) => {
+                if (index === 0 && delimeter.startsWith('[')) { // remove first "[" in first element
+                    delimeter = delimeter.replace('[', '');
+                }
+                if (index === delimeterArray.length - 1 && delimeter.endsWith(']')) { // remove last "]" in last element
+                    delimeter = delimeter.replace(/\](?=[^\]]*$)/g, '');
+                }
+                this.delimeters.push(delimeter);
+            });
+        }
+        return stringWithoutAdditionalDelimeters;
+    }
+
+    checkIfStringContainsValidDelimeters(string = '') {
+        for (let stringIndex = 0; stringIndex < string.length; stringIndex += 1) {
+            if (isNaN(string.charAt(stringIndex))) {
+                let delimeterFound = false;
+                for (const delimeter of this.delimeters) {
+                    let substringIndex = stringIndex;
+                    let substring = string.substring(substringIndex, substringIndex + 1);
+                    while (delimeter.startsWith(substring) && substring.length < delimeter.length) {
+                        substringIndex++;
+                        const newSubstring = string.substring(substringIndex, substringIndex + 1);
+                        if (newSubstring === '') {
+                            break;
+                        }
+                        substring += string.substring(substringIndex, substringIndex + 1);
+                    }
+                    console.log('DELIMETER: ', delimeter, ' SUBSTRING: ', substring);
+                    if (delimeter === substring) {
+                        delimeterFound = true;
+                    }
+                }
+                if (!delimeterFound) {
+                    throw new Error('Unsupported delimeter found!');
+                }
+            }
+        }
+    }
+
+
 
 
     validateString(string = '') {
         if (typeof string !== 'string') {
             throw new Error('The input type must be string');
         }
-        const multiDimensionSplit = string.split('\n').map((line) => this.processDelimiter(line));
+        const stringWithoutAdditionalDelimeters = this.validateAndReplaceDelimeterFromTheString(string);
+        this.checkIfStringContainsValidDelimeters(stringWithoutAdditionalDelimeters);
+        const multiDimensionSplit = stringWithoutAdditionalDelimeters.split('\n').map((line) => this.processDelimiter(line));
         const stringFormedByMultiDimensionSplit = multiDimensionSplit.join(',').replace(/\[/g, '').replace(/\]/g, '');
         const negativeNumbers = [];
         stringFormedByMultiDimensionSplit.split(',').forEach((stringItem) => {
@@ -55,7 +108,7 @@ class StringCalculator {
 
 
     calculateSum() {
-        return this.dimensionalStringSplit.split(',').reduce((a, b) => parseInt(a) + parseInt(b) || 0 , 0);
+        return this.dimensionalStringSplit.split(',').reduce((a, b) => parseInt(a) + parseInt(b) || 0, 0);
     }
 }
 
@@ -69,16 +122,23 @@ const stringCalculatorClientFunction = (string) => {
 }
 
 
-stringCalculatorClientFunction('');
-stringCalculatorClientFunction('1,2,3,4');
-stringCalculatorClientFunction('1,2,3,a');
-stringCalculatorClientFunction('1,2,,4');
-stringCalculatorClientFunction('12,13,14,15');
-stringCalculatorClientFunction('12,13,-14,15,-16');
-stringCalculatorClientFunction(null);
-stringCalculatorClientFunction(undefined);
-stringCalculatorClientFunction(false);
-stringCalculatorClientFunction('1,2,3\n4');
-stringCalculatorClientFunction('1,2,\n');
-stringCalculatorClientFunction('1,2,');
-stringCalculatorClientFunction('1,2,3,4,5+6+7+8*9*10*11\n12,13,14+15');
+// stringCalculatorClientFunction('');
+// stringCalculatorClientFunction('1,2,3,4');
+// stringCalculatorClientFunction('1,2,3,a');
+// stringCalculatorClientFunction('1,2,,4');
+// stringCalculatorClientFunction('12,13,14,15');
+// stringCalculatorClientFunction('12,13,-14,15,-16');
+// stringCalculatorClientFunction(null);
+// stringCalculatorClientFunction(undefined);
+// stringCalculatorClientFunction(false);
+// stringCalculatorClientFunction('1,2,3\n4');
+// stringCalculatorClientFunction('1,2,\n');
+// stringCalculatorClientFunction('1,2,');
+// stringCalculatorClientFunction('1,2,3,4,5+6+7+8*9*10*11\n12,13,14+15');
+// stringCalculatorClientFunction('//[,][+][*]\n1,2,3,4,5+6+7+8*9*10*11\n12,13,14+15');
+// stringCalculatorClientFunction('//[,][*]\n1,2,3,4,5+6+7+8*9*10*11\n12,13,14+15');
+// stringCalculatorClientFunction('//[,][+][*]\n1,2,3,4,5+6+7+-8*9*10*11\n12,13,14+15');
+// stringCalculatorClientFunction('//[,][+][*]1,2,3,4,5+6+7+8*9*10*11\n12,13,14+15');
+// stringCalculatorClientFunction('//[,,][+][*]\n1,2,3,4,5+6+7+8*9*10*11\n12,13,14+15');
+stringCalculatorClientFunction('//[,,][+][*]\n1,,2,,3,,4,,5+6+7+8*9*10*11\n12,,13,,14+15');
+
