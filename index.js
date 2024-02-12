88db8d06feb9116bb6c3222c60c60f97f2b4c563
Bhavya Dhiman <bhavyadhiman7@gmail.com>
2024-02-13 01:36:37 +0530

    feat(string-calculator): multiple dimensions handling calculations and refactoring

--- index.js
+++ index.js
@@ -1,30 +1,15 @@
 class StringCalculator {
     constructor(string = '') {
         this.delimeters = [',', '+', '*'];
-        this.validateString(string);
-        this.string =  string || '';
+        this.dimensionalStringSplit = this.validateString(string);
+        this.string = string || '';
     }
 
-    checkIfStringContainsNumbersOnly(string = '') {
-        string.split(this.delimeters).forEach(stringItem => {
-            if (isNaN(Number(stringItem))) {
-                throw new Error('String should contain only numbers!');
-            }
-        })
-    }
-
-    checkIfStringEndsWithNumber(string = '') {
-        if (string !== '' && string.endsWith('\n') || string.endsWith(this.delimeters)) {
-            throw new Error('String should end with a number!');
+    processDelimiter(string, index = 0) {
+        if (index > this.delimeters.length - 1) {
+            return string;
         }
-    }
-
-    checkIfStringIsEmptyBetweenTwoDelimeters(string = '') {
-        string.split(this.delimeters).forEach(stringItem => {
-           if (stringItem === '') {
-             throw new Error('Between two delimeters, string should not be empty');
-           }
-        })
+        return string.split(this.delimeters[index]).map(s => this.processDelimiter(s, index + 1));
     }
 
     checkIfStringContainsNegativeNumbers(string = '') {
@@ -42,48 +27,45 @@ class StringCalculator {
 
 
     validateString(string = '') {
-       if (typeof string !== 'string') {
-          throw new Error('The input type must be string');
-       }
-       this.checkIfStringEndsWithNumber(string);
-       this.checkIfStringContainsNumbersOnly(string);
-       this.checkIfStringContainsNegativeNumbers(string);
-    }
-
-
-    processArray(array, sum) {
-        if (!Array.isArray(array)) {
-            sum += parseInt(array) || 0;
-            return sum;
+        if (typeof string !== 'string') {
+            throw new Error('The input type must be string');
         }
-        array.forEach((arrayElement) => {
-           sum = this.processArray(arrayElement, sum);
+        const multiDimensionSplit = string.split('\n').map((line) => this.processDelimiter(line));
+        const stringFormedByMultiDimensionSplit = multiDimensionSplit.join(',').replace(/\[/g, '').replace(/\]/g, '');
+        const negativeNumbers = [];
+        stringFormedByMultiDimensionSplit.split(',').forEach((stringItem) => {
+            if (isNaN(Number(stringItem))) {
+                throw new Error('String should contain only numbers!');
+            }
+            if (stringItem !== '' && stringItem.endsWith('\n') || stringItem.endsWith(this.delimeters)) {
+                throw new Error('String should end with a number!');
+            }
+            if (stringItem === '' && stringFormedByMultiDimensionSplit.length) {
+                throw new Error('Between delimeters, string should not be empty');
+            }
+            if (Number(stringItem) < 0) {
+                negativeNumbers.push(stringItem);
+            }
         });
-        return sum;
-    }
-    
-
-    processDelimiter(string, index) {
-        if (index > this.delimeters.length - 1) {
-            return string;
+        if (negativeNumbers.length) {
+            throw new Error(`negative numbers are not allowed: ${negativeNumbers.join(',')}`);
         }
-       return string.split(this.delimeters[index]).map(s=> this.processDelimiter(s, index + 1));
+        return stringFormedByMultiDimensionSplit;
     }
 
-    addNumbersInString() {
-        this.validateString();
-        const multiDimensionArray = this.string.split('\n').map((line) => this.processDelimiter(line, 0));
-        return this.processArray(multiDimensionArray, 0);
+
+    calculateSum() {
+        return this.dimensionalStringSplit.split(',').reduce((a, b) => parseInt(a) + parseInt(b) || 0 , 0);
     }
 }
 
 const stringCalculatorClientFunction = (string) => {
-   try {
-    const client = new StringCalculator(string);
-    console.log(client.addNumbersInString(string));
-   } catch (error) {
-      console.log(`ERROR in ${string}:`, error.message);
-   }
+    try {
+        const client = new StringCalculator(string);
+        console.log(client.calculateSum(string));
+    } catch (error) {
+        console.log(`ERROR in ${string}:`, error.message);
+    }
 }
 
 
@@ -96,7 +78,7 @@ stringCalculatorClientFunction('12,13,-14,15,-16');
 stringCalculatorClientFunction(null);
 stringCalculatorClientFunction(undefined);
 stringCalculatorClientFunction(false);
-stringCalculatorClientFunction('1,2,3\n4,5,6\n7,8,9');
+stringCalculatorClientFunction('1,2,3\n4');
 stringCalculatorClientFunction('1,2,\n');
 stringCalculatorClientFunction('1,2,');
-stringCalculatorClientFunction('1,2,3,4,5+6+7+8*9*10*11');
+stringCalculatorClientFunction('1,2,3,4,5+6+7+8*9*10*11\n12,13,14+15');
